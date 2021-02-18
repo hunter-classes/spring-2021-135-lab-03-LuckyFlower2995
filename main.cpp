d236045e70358c6bbdd57553a2f0fc8e028100d8
Stephanie Lam <lamstephanie89@gmail.com>
2021-02-17 21:20:58 -0500

    completed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
-// add your code as appropriate 
 #include <iostream>
+#include "reservoir.h"
+#include "reverseorder.h"
+
+using namespace std;
 
 int main()
 {
-  
-
-  
+   string date = "01/01/2018";
+   string date1 = "05/29/2018";
+   string date2 = "06/02/2018";
+   
+   get_east_storage(date);
+   cout << endl;
+   
+   get_min_east();
+   get_max_east();
+   cout << endl;
+   
+   compare_basins(date);
+   cout << endl;
+   
+   reverse_order(date1, date2);
   return 0;
 }
--- reservoir.cpp
+++ reservoir.cpp
@@ -1,1 +1,124 @@
-// add your code
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include <iostream>
+#include "reservoir.h"
+
+using namespace std;
+
+double get_east_storage(std::string date) { 
+   string date2 = date;
+   string eastSt;
+   
+   ifstream fin("Current_Reservoir_Levels.tsv");
+   
+   if(fin.fail()) {
+      cerr << "File cannot be opened for reading." << endl;
+      exit(1);
+   }
+   string junk;
+   getline(fin, junk);
+   
+   while(fin >> date >> eastSt) {
+      fin.ignore(INT_MAX, '\n');
+      if (date == date2) {
+         cout << date << ": " << eastSt << " billion gallons." << endl;
+      }
+   }  
+   fin.close();
+   return 0;
+}
+
+//////////////////////////////////////////////////////////////
+
+double get_min_east() { 
+   double eastSt;
+   string date;
+   double min = 100;
+   
+   
+   ifstream fin("Current_Reservoir_Levels.tsv");
+   
+   if(fin.fail()) {
+      cerr << "File cannot be opened for reading." << endl;
+      exit(1);
+   }
+   string junk;
+   getline(fin, junk);
+   
+   while(fin >> date >> eastSt) {
+      fin.ignore(INT_MAX, '\n');
+      if (eastSt < min) {
+         min = eastSt;
+      }
+  
+   }  
+   cout << "Minimum storage in East Basin: " << min
+        << " billion gallons.\n";
+   fin.close();
+   return 0;
+}
+
+////////////////////////////////////////////////////////////////
+
+double get_max_east() { 
+   double eastSt;
+   string date;
+   double max = 0;
+   
+   
+   ifstream fin("Current_Reservoir_Levels.tsv");
+   
+   if(fin.fail()) {
+      cerr << "File cannot be opened for reading." << endl;
+      exit(1);
+   }
+   string junk;
+   getline(fin, junk);
+   
+   while(fin >> date >> eastSt) {
+      fin.ignore(INT_MAX, '\n');
+      if (eastSt > max) {
+         max = eastSt;
+      }
+  
+   }  
+   cout << "Maximum storage in East Basin: " << max
+        << " billion gallons.\n";
+   fin.close();
+   return 0;
+}
+
+///////////////////////////////////////////////////////////
+
+string compare_basins(std::string date) { 
+   string date2 = date;
+   double eastSt;
+   double eastEl;
+   double westSt;
+   double westEl;
+   
+   ifstream fin("Current_Reservoir_Levels.tsv");
+   
+   if(fin.fail()) {
+      cerr << "File cannot be opened for reading." << endl;
+      exit(1);
+   }
+   string junk;
+   getline(fin, junk);
+   
+   while(fin >> date >> eastSt >> eastEl >> westSt >> westEl) {
+      fin.ignore(INT_MAX, '\n');
+      if (date == date2) {
+         if (eastEl > westEl) {
+            cout << "East\n";
+         } else if (eastEl < westEl) {
+            cout << "West\n";
+         } else {
+            cout << "Equal\n";
+         }
+      }
+   }  
+   fin.close();
+   return(" ");
+}
